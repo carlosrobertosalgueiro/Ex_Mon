1fc6994af3bce5efb3267a07759f99e6bae608a1
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-03-10 19:04:10 -0300

    inserted player

---
+++ lib/player.ex
@@ -0,0 +1,18 @@
+defmodule ExMon.Player do
+  requered_keys = [:file, :move_avg, :move_heal, :move_rnd, :name]
+  @max_life 100
+
+  @enforce_keys requered_keys
+
+  defstruct requered_keys
+
+  def build(move_avg, move_heal, move_rnd, name) do
+    %ExMon.Player{
+      file: @max_life,
+      move_avg: move_avg,
+      move_heal: move_heal,
+      move_rnd: move_rnd,
+      name: name
+    }
+  end
+end
