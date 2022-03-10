a9831b99d4bc445c28c7a2d7f2d0f1ff48202f73
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-03-10 19:29:33 -0300

    frist call

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,7 @@
 defmodule ExMon do
-  def hello do
-    :world
+  alias ExMon.Player
+
+  def create_player(move_avg, move_heal, move_rnd, name) do
+    Player.build(move_avg, move_heal, move_rnd, name)
   end
 end
