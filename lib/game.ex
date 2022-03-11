3dd9b77ae3ffc7395a200cc509325b865d18242b
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-03-11 13:16:12 -0300

    printing game state

--- lib/game.ex
+++ lib/game.ex
@@ -1,9 +1,11 @@
 defmodule ExMon.Game do
   use Agent
+  alias ExMon.Game.Status
 
   def start(computer, player) do
     initial_value = %{computer: computer, player: player, turn: :player, status: :started}
     Agent.start_link(fn -> initial_value end, name: __MODULE__)
+    Status.print_round_menssage()
   end
 
   def info do
