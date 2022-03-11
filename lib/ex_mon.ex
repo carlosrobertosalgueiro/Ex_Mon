987d8c56e7a1fdfb8d9a20bb4500cb9f894edd7f
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-03-11 07:17:06 -0300

    inserted call player

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,13 @@
 defmodule ExMon do
   alias ExMon.Player
+  alias ExMon.Game
 
   def create_player(move_avg, move_heal, move_rnd, name) do
     Player.build(move_avg, move_heal, move_rnd, name)
+  end
+
+  def start_game(player) do
+    computer = create_player("PC_Carlos", :punch, :kick, :heal)
+    Game.start(computer, player)
   end
 end
