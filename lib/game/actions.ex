063d61041c08142c9515961773594c2b29ec0d25
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-03-12 12:57:46 -0300

    starting attack function

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -12,4 +12,8 @@ defmodule ExMon.Game.Actions do
       if value == move, do: {:ok, key}
     end)
   end
+
+  def attack(move) do
+    IO.puts("\n====== Movimento de atque #{move}. =====\n")
+  end
 end
