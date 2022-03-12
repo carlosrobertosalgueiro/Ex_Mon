7e3fd6eadbbf37f8338223d4870e890547a53823
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-03-12 10:49:12 -0300

    Refactor Map

--- lib/player.ex
+++ lib/player.ex
@@ -1,5 +1,5 @@
 defmodule ExMon.Player do
-  requered_keys = [:file, :move_avg, :move_heal, :move_rnd, :name]
+  requered_keys = [:file, :moves, :name]
   @max_life 100
 
   @enforce_keys requered_keys
@@ -9,9 +9,11 @@ defmodule ExMon.Player do
   def build(name, move_avg, move_heal, move_rnd) do
     %ExMon.Player{
       file: @max_life,
-      move_avg: move_avg,
-      move_heal: move_heal,
-      move_rnd: move_rnd,
+      moves: %{
+        move_avg: move_avg,
+        move_heal: move_heal,
+        move_rnd: move_rnd
+      },
       name: name
     }
   end
