40948c050fc9c636f02791306a1ba300c2868009
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-03-20 23:40:36 -0300

    inserted function thaht show attake value

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -1,5 +1,13 @@
 defmodule ExMon.Game.Actions do
   alias ExMon.Game
+  alias ExMon.Game.Actions.Attack
+
+  def attack(move) do
+    case Game.turn() do
+      :player -> Attack.attack_opponent(:computer, move)
+      :computer -> Attack.attack_opponent(:player, move)
+    end
+  end
 
   def fetch_move(move) do
     Game.player()
@@ -11,9 +19,5 @@ defmodule ExMon.Game.Actions do
     Enum.find_value(moves, {:error, move}, fn {key, value} ->
       if value == move, do: {:ok, key}
     end)
-  end
-
-  def attack(move) do
-    IO.puts("\n====== Movimento de atque #{move}. =====\n")
   end
 end
