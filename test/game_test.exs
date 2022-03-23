6b05042ab0772c85116428885dcad088ae7bd5e9
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-03-23 16:55:04 -0300

    testing info function

--- test/game_test.exs
+++ test/game_test.exs
@@ -11,4 +11,30 @@ defmodule ExMon.GameTest do
       assert {:ok, _pid} = Game.start(player, computer)
     end
   end
+
+  describe "info/0" do
+    test "returns the current game state" do
+      player = ExMon.create_player("carlos", :soco, :chute, :cura)
+      computer = ExMon.create_player("PC_carlos", :soco, :chute, :cura)
+
+      Game.start(player, computer)
+
+      expected_response = %{
+        computer: %Player{
+          life: 100,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "carlos"
+        },
+        player: %Player{
+          life: 100,
+          moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+          name: "PC_carlos"
+        },
+        status: :started,
+        turn: :player
+      }
+
+      assert expected_response == Game.info()
+    end
+  end
 end
