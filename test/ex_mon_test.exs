30300de55da9526e02e137a84b619b871fe6ac2b
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-03-25 14:18:21 -0300

    inserted test of start game function

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -1,6 +1,10 @@
 defmodule ExMonTest do
   use ExUnit.Case
 
+  import ExUnit.CaptureIO
+
+  alias ExMon.Player
+
   describe "create_player/4" do
     test "returns a player" do
       expected_response = %ExMon.Player{
@@ -10,6 +14,20 @@ defmodule ExMonTest do
       }
 
       assert expected_response == ExMon.create_player("carlos", :soco, :chute, :cura)
+    end
+  end
+
+  describe "start_game/1" do
+    test "when the game is started, returns a message" do
+      player = Player.build("carlos", :soco, :chute, :cura)
+
+      messages =
+        capture_io(fn ->
+          assert ExMon.start_game(player) == :ok
+        end)
+
+      assert messages =~ "The game is started"
+      assert messages =~ "status: :started"
     end
   end
 end
