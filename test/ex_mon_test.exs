29e9ca485d3ad8bc135ff0e3420f6349d96c10f3
carlosrobertosalgueiro <carlosrobertounopar01@hotmail.com>
2022-03-25 12:42:11 -0300

    inserted test of function created player

--- test/ex_mon_test.exs
+++ test/ex_mon_test.exs
@@ -1,7 +1,15 @@
 defmodule ExMonTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert true == true
+  describe "create_player/4" do
+    test "returns a player" do
+      expected_response = %ExMon.Player{
+        life: 100,
+        moves: %{move_avg: :soco, move_heal: :cura, move_rnd: :chute},
+        name: "carlos"
+      }
+
+      assert expected_response == ExMon.create_player("carlos", :soco, :chute, :cura)
+    end
   end
 end
